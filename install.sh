ce18f4d5686329488c7571864198ceea5a2445ad
Brian Chevalier <brianchevalier@gmail.com>
2019-04-21 22:00:59 -0700

    Now adding template files for ipynb export

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ sudo cp -fr *.sty $TEXMF
 sudo cp -fr *.cls $TEXMF
 sudo cp -fr *.tex $TEXMF
 sudo texhash
+
+mkdir -p ~/.jupyter/templates       # make templates folder if not existing
+cp -fr *.tplx ~/.jupyter/templates  # copy template files to path
